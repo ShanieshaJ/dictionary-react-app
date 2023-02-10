6fc67cc6bd3c6671874528ce0355f0759e1bff8d
Shaniesha John <shaniesha.john.94@gmail.com>
2023-02-10 00:30:38 -0500

    Edited the search bar

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,13 +5,19 @@ export default function Dictionary() {
   return (
     <div className="Dictionary">
       <section>
-        <form className="dictionarySearch">
-          <input
-            className="input-text"
-            type="search"
-            placeholder="Type a word..."
-          ></input>
-          <input className="input-submit" type="submit" value="Search"></input>
+        <form className="form-control">
+          <div className="row">
+            <div className="col-9 p-1">
+              <input
+                type="text"
+                className="search"
+                placeholder="Search for a word"
+              />
+            </div>
+            <div className="col-1 p-1">
+              <button className="btn btn-primary">🔍</button>
+            </div>
+          </div>
         </form>
       </section>
     </div>
